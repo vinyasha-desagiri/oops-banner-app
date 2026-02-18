6e1d4148e811a40ea0a8c2cc7356618efcc515f0
vinyasha-desagiri <vd8656@srmist.edu.in>
2026-02-18 11:39:23 +0530

    ur1-Print Banner

---
+++ OOPSBannerApp.java
@@ -0,0 +1,11 @@
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("********************************");
+        System.out.println("        OOPS BANNER APP         ");
+        System.out.println("     Welcome to Java Program    ");
+        System.out.println("********************************");
+
+    }
+}
